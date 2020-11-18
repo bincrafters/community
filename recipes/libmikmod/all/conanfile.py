43287074e26ca9bc048e419397312becb409b01c
Croydon <git@cr0ydon.com>
2020-11-18 14:18:57 +0100

    libmikmod: use self.options.get_safe()

--- recipes/libmikmod/all/conanfile.py
+++ recipes/libmikmod/all/conanfile.py
@@ -73,21 +73,14 @@ class LibmikmodConan(ConanFile):
         cmake = CMake(self, set_cmake_flags=True)
         cmake.definitions["ENABLE_STATIC"] = not self.options.shared
         cmake.definitions["ENABLE_DOC"] = False
-        cmake.definitions["ENABLE_DSOUND"] = self._get_safe_bool("with_dsound")
-        cmake.definitions["ENABLE_MMSOUND"] = self._get_safe_bool("with_mmsound")
-        cmake.definitions["ENABLE_ALSA"] = self._get_safe_bool("with_alsa")
-        cmake.definitions["ENABLE_OSS"] = self._get_safe_bool("with_oss")
-        cmake.definitions["ENABLE_PULSE"] = self._get_safe_bool("with_pulse")
-        cmake.definitions["ENABLE_COREAUDIO"] = self._get_safe_bool("with_coreaudio")
+        cmake.definitions["ENABLE_DSOUND"] = self.options.get_safe("with_dsound", False)
+        cmake.definitions["ENABLE_MMSOUND"] = self.options.get_safe("with_mmsound", False)
+        cmake.definitions["ENABLE_ALSA"] = self.options.get_safe("with_alsa", False)
+        cmake.definitions["ENABLE_OSS"] = self.options.get_safe("with_oss", False)
+        cmake.definitions["ENABLE_PULSE"] = self.options.get_safe("with_pulse", False)
+        cmake.definitions["ENABLE_COREAUDIO"] = self.options.get_safe("with_coreaudio", False)
         cmake.configure(build_folder=self._build_subfolder, source_folder=self._source_subfolder)
         return cmake
-
-    def _get_safe_bool(self, option_name):
-        opt = self.options.get_safe(option_name)
-        if opt is not None:
-            return opt
-        else:
-            return False
 
     def build(self):
         # 0001:
@@ -122,9 +115,9 @@ class LibmikmodConan(ConanFile):
         if not self.options.shared:
             self.cpp_info.defines = ["MIKMOD_STATIC"]
 
-        if self._get_safe_bool("with_dsound"):
+        if self.options.get_safe("with_dsound"):
             self.cpp_info.system_libs.append("dsound")
-        if self._get_safe_bool("with_mmsound"):
+        if self.options.get_safe("with_mmsound"):
             self.cpp_info.system_libs.append("winmm")
-        if self._get_safe_bool("with_coreaudio"):
+        if self.options.get_safe("with_coreaudio"):
             self.cpp_info.frameworks.append("CoreAudio")
