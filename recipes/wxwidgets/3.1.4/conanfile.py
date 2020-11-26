80ac9f29d76c62bf9189e6d9b7b45b56d95d3e03
Croydon <git@cr0ydon.com>
2020-11-26 10:38:50 +0100

    wxwidgets: update libjpeg-turbo to 2.0.6

--- recipes/wxwidgets/3.1.4/conanfile.py
+++ recipes/wxwidgets/3.1.4/conanfile.py
@@ -124,7 +124,7 @@ class wxWidgetsConan(ConanFile):
         if self.options.jpeg == 'libjpeg':
             self.requires('libjpeg/9d')
         elif self.options.jpeg == 'libjpeg-turbo':
-            self.requires('libjpeg-turbo/2.0.5')
+            self.requires('libjpeg-turbo/2.0.6')
         elif self.options.jpeg == 'mozjpeg':
             self.requires('mozjpeg/3.3.1')
         if self.options.tiff == 'libtiff':
