edb20d74b89f1ce88e237aa1287502f58ada9930
ericLemanissier <ericLemanissier@users.noreply.github.com>
2020-12-31 18:35:44 +0100

    Update recipes/sdl2/all/conanfile.py

--- recipes/sdl2/all/conanfile.py
+++ recipes/sdl2/all/conanfile.py
@@ -270,10 +270,10 @@ class SDL2Conan(ConanFile):
         if self.settings.os == "Linux":
             if self.options.pulse:
                 os.rename("libpulse.pc", "libpulse-simple.pc")
-        with tools.run_environment(self):
-            with tools.environment_append({"LIBRARY_PATH": os.getenv("LD_LIBRARY_PATH")}):
-                cmake = self._configure_cmake()
-                cmake.build()
+        lib_paths = [lib for dep in self.deps_cpp_info.deps for lib in self.deps_cpp_info[dep].lib_paths]
+        with tools.environment_append({"LIBRARY_PATH": os.pathsep.join(lib_paths)}):
+            cmake = self._configure_cmake()
+            cmake.build()
 
     def package(self):
         self.copy(pattern="COPYING.txt", dst="license", src=self._source_subfolder)
