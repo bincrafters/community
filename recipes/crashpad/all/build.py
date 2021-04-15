e2056c26f04fde78a2f7979795a755a4121783e6
Croydon <git@cr0ydon.com>
2021-04-15 14:35:24 +0200

    build.py: try installing python2.7-minimal

--- recipes/crashpad/all/build.py
+++ recipes/crashpad/all/build.py
@@ -8,10 +8,9 @@ from bincrafters import build_template_default
 if __name__ == "__main__":
 
     if platform.system() == "Linux":
-        command = os.environ["CONAN_DOCKER_ENTRY_SCRIPT"] + "; sudo apt-get -qq update && sudo apt-get -qq install -y python-minimal"
+        command = os.environ["CONAN_DOCKER_ENTRY_SCRIPT"] + "; sudo apt-get -qq update && sudo apt-get -qq install -y python2.7-minimal"
         builder = build_template_default.get_builder(docker_entry_script=command, pure_c=False)
     else:
         builder = build_template_default.get_builder(pure_c=False)
 
     builder.run()
-    
