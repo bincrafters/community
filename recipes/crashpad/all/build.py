9884057009dde5fb892c7c48491ffde149f3b73e
Michael Keck <git@cr0ydon.com>
2021-04-15 15:26:58 +0200

    add crashpad/20200508 (#1381)
    * add crashpad/20200508
    
    Migrated from https://github.com/bincrafters/conan-crashpad
    
    * Try deleting build.py
    
    * Revert "Try deleting build.py"
    
    This reverts commit 460b016303b4591224ee2dd1b480a7a5176758e7.
    
    * build.py: try installing python2.7-minimal

---
+++ recipes/crashpad/all/build.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import platform
+from bincrafters import build_template_default
+
+if __name__ == "__main__":
+
+    if platform.system() == "Linux":
+        command = os.environ["CONAN_DOCKER_ENTRY_SCRIPT"] + "; sudo apt-get -qq update && sudo apt-get -qq install -y python2.7-minimal"
+        builder = build_template_default.get_builder(docker_entry_script=command, pure_c=False)
+    else:
+        builder = build_template_default.get_builder(pure_c=False)
+
+    builder.run()
