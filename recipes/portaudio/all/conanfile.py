03c1a9c3d95e6e7bfe8d989d7ee94e00b311f3ef
Croydon <git@cr0ydon.com>
2021-07-14 07:09:35 +0200

    Use CMake also for Linux and macOS builds

--- recipes/portaudio/all/conanfile.py
+++ recipes/portaudio/all/conanfile.py
@@ -65,63 +65,19 @@ class PortaudioConan(ConanFile):
     def source(self):
         tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
 
-    def patch_source(self):
-        if self.settings.os == "Macos":
-            tools.replace_in_file(os.path.join(self._source_subfolder, "configure"), 'mac_sysroot="-isysroot `xcodebuild -version -sdk macosx10.12 Path`"',
-"""
-mac_sysroot="-isysroot `xcodebuild -version -sdk macosx10.12 Path`"
-elif xcodebuild -version -sdk macosx10.13 Path >/dev/null 2>&1 ; then
-                 mac_version_min="-mmacosx-version-min=10.4"
-                 mac_sysroot="-isysroot `xcodebuild -version -sdk macosx10.13 Path`"
-elif xcodebuild -version -sdk macosx10.14 Path >/dev/null 2>&1 ; then
-                 mac_version_min="-mmacosx-version-min=10.4"
-                 mac_sysroot="-isysroot `xcodebuild -version -sdk macosx10.14 Path`"
-
-"""
-                        )
-            tools.replace_in_file(os.path.join(self._source_subfolder, "configure"), "Could not find 10.5 to 10.12 SDK.", "Could not find 10.5 to 10.14 SDK.")
-        elif self.settings.os == "Windows" and self.settings.compiler == "gcc":
-            tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"), 'OPTION(PA_USE_WDMKS "Enable support for WDMKS" ON)', 'OPTION(PA_USE_WDMKS "Enable support for WDMKS" OFF)')
-            tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"), 'OPTION(PA_USE_WDMKS_DEVICE_INFO "Use WDM/KS API for device info" ON)', 'OPTION(PA_USE_WDMKS_DEVICE_INFO "Use WDM/KS API for device info" OFF)')
-            tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"), 'OPTION(PA_USE_WASAPI "Enable support for WASAPI" ON)', 'OPTION(PA_USE_WASAPI "Enable support for WASAPI" OFF)')
-
     def _configure_cmake(self):
         if not self._cmake:
             self._cmake = CMake(self)
             self._cmake.definitions["PA_BUILD_STATIC"] = not self.options.shared
             self._cmake.definitions["PA_BUILD_SHARED"] = self.options.shared
+            self._cmake.definitions["PA_USE_JACK"] = self.options.get_safe("with_jack", False)
+            self._cmake.definitions["PA_USE_ALSA"] = self.options.get_safe("with_alsa", False)
             self._cmake.configure()
         return self._cmake
 
     def build(self):
-        self.patch_source()
-
-        if self.settings.os == "Linux" or self.settings.os == "Macos":
-            env = AutoToolsBuildEnvironment(self)
-            env.fpic = self.options.fPIC
-            args = []
-            if self.options.cpp_bindings:
-                args.append("--enable-cxx")
-            if self.settings.os == "Macos" and self.settings.compiler == "apple-clang":
-                args.append("--disable-mac-universal")
-            elif self.settings.os == "Linux":
-                args.append("--with-alsa" if self.options.with_alsa else "--without-alsa")
-                args.append("--with-jack" if self.options.with_jack else "--without-jack")
-                if self.options.with_alsa:
-                    env.flags.extend("-I%s" % p for p in self.deps_cpp_info["libalsa"].include_paths) # env.include_paths does not seem to work here
-            env.configure(configure_dir=self._source_subfolder, args=args)
-            if self.settings.os == "Macos" and self.settings.compiler == "apple-clang":
-                env_args = []
-                if self.options.cpp_bindings:
-                    env_args.append("-j1")
-                env.make(args=env_args)
-            else:
-                env.make(target = "lib/libportaudio.la")
-            if self.settings.os == "Macos" and self.options.shared:
-                self.run('cd lib/.libs && for filename in *.dylib; do install_name_tool -id $filename $filename; done')
-        else:
-            cmake = self._configure_cmake()
-            cmake.build()
+        cmake = self._configure_cmake()
+        cmake.build()
 
     def package(self):
         self.copy(pattern="LICENSE*", dst="licenses", src=self._source_subfolder,  ignore_case=True, keep_path=False)
@@ -146,12 +102,12 @@ elif xcodebuild -version -sdk macosx10.14 Path >/dev/null 2>&1 ; then
         self.cpp_info.libs = tools.collect_libs(self)
 
         if self.settings.os == "Macos":
-            self.cpp_info.frameworks.extend(["CoreAudio","AudioToolbox","AudioUnit","CoreServices","Carbon"])
+            self.cpp_info.frameworks.extend(["CoreAudio", "AudioToolbox", "AudioUnit", "CoreServices", "Carbon"])
 
         if self.settings.os == "Windows" and self.settings.compiler == "gcc" and not self.options.shared:
-            self.cpp_info.system_libs.append('winmm')
+            self.cpp_info.system_libs.append("winmm")
 
         if self.settings.os == "Linux" and not self.options.shared:
-            self.cpp_info.system_libs.extend(['m', 'pthread'])
+            self.cpp_info.system_libs.extend(["m", "pthread"])
             if self.options.with_jack:
-                self.cpp_info.system_libs.append('jack')
+                self.cpp_info.system_libs.append("jack")
