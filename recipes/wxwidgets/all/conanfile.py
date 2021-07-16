dd22e872dcb2ccca0fc41a641da84448ca0c2914
Eric Lemanissier <e.lemanissier@physioflow.com>
2021-07-16 10:33:42 +0200

    fix macos

--- recipes/wxwidgets/all/conanfile.py
+++ recipes/wxwidgets/all/conanfile.py
@@ -346,7 +346,8 @@ class wxWidgetsConan(ConanFile):
                               'Security',
                               'ImageIO',
                               'System',
-                              'WebKit']:
+                              'WebKit',
+                              'QuartzCore']:
                 self.cpp_info.frameworks.append(framework)
         elif self.settings.os == 'Windows':
             # see cmake/init.cmake
