60cdbb2d2707414cff79be3a591e26d01095a297
Cr0ydon <git@cr0ydon.com>
2021-08-08 08:34:29 +0200

    Actual copy license file

--- recipes/portaudio/all/conanfile.py
+++ recipes/portaudio/all/conanfile.py
@@ -83,7 +83,7 @@ class PortaudioConan(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="LICENSE*", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
         tools.rmdir(os.path.join(self.package_folder, "share"))
