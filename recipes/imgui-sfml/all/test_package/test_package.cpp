826bddf74c4c62b50a7e89095baf192ac720d635
Gryfenfer97 <hajjarjamil4@gmail.com>
2021-12-12 01:24:09 +0100

    add imgui-sfml v2.4 & v2.5

--- recipes/imgui-sfml/all/test_package/test_package.cpp
+++ recipes/imgui-sfml/all/test_package/test_package.cpp
@@ -2,9 +2,12 @@
 #include "imgui-SFML.h"
 
 #include <SFML/Window/Event.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
 
 int main() {
     sf::Event event; 
-    ImGui::SFML::ProcessEvent(event);
+    sf::RenderWindow window(sf::VideoMode(1, 1), "");
+    ImGui::SFML::Init(window);
+    ImGui::SFML::ProcessEvent(window, event);
     return 0;
 }
