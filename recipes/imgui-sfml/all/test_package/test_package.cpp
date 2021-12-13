2407a52b43fdcbe28babb74b1069380609f1f951
Gryfenfer97 <hajjarjamil4@gmail.com>
2021-12-13 08:14:11 +0100

    add conditional compilation

--- recipes/imgui-sfml/all/test_package/test_package.cpp
+++ recipes/imgui-sfml/all/test_package/test_package.cpp
@@ -2,12 +2,18 @@
 #include "imgui-SFML.h"
 
 #include <SFML/Window/Event.hpp>
+#ifdef WITH_WINDOW
 #include <SFML/Graphics/RenderWindow.hpp>
+#endif
 
 int main() {
     sf::Event event; 
+    #ifdef WITH_WINDOW
     sf::RenderWindow window(sf::VideoMode(1, 1), "");
     ImGui::SFML::Init(window);
     ImGui::SFML::ProcessEvent(window, event);
+    #else
+    ImGui::SFML::ProcessEvent(event);
+    #endif
     return 0;
 }
